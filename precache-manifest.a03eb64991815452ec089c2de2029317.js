354f54b7c260d1221e5f7a9100c1bdb388521c1e
unknown <agetorgodsway@gmail.com>
2020-09-08 15:06:57 +0100

    Updates

---
+++ precache-manifest.a03eb64991815452ec089c2de2029317.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f444ccfc46f357fcc0da06654cb370",
+    "url": "/github.com/cov3nant/HW3_KWG7CSHTMC.git/index.html"
+  },
+  {
+    "revision": "be4215516924894119dc",
+    "url": "/github.com/cov3nant/HW3_KWG7CSHTMC.git/static/css/main.1b734b52.chunk.css"
+  },
+  {
+    "revision": "d9937a6f072d24cd195a",
+    "url": "/github.com/cov3nant/HW3_KWG7CSHTMC.git/static/js/2.e98d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.com/cov3nant/HW3_KWG7CSHTMC.git/static/js/2.e98d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4215516924894119dc",
+    "url": "/github.com/cov3nant/HW3_KWG7CSHTMC.git/static/js/main.9f1a6bd8.chunk.js"
+  },
+  {
+    "revision": "763b22250b8cb25cd0d9",
+    "url": "/github.com/cov3nant/HW3_KWG7CSHTMC.git/static/js/runtime-main.ff72d18f.js"
+  },
+  {
+    "revision": "5f1cd582c6d4fb5f4d86e6c1635deec2",
+    "url": "/github.com/cov3nant/HW3_KWG7CSHTMC.git/static/media/hulk-1.5f1cd582.jpg"
+  },
+  {
+    "revision": "294dd39b7ab19aedb004eb8f359c307f",
+    "url": "/github.com/cov3nant/HW3_KWG7CSHTMC.git/static/media/venom-1.294dd39b.jpg"
+  },
+  {
+    "revision": "5eb62fe8d11e8a32b2fb2554088f308e",
+    "url": "/github.com/cov3nant/HW3_KWG7CSHTMC.git/static/media/venom-2.5eb62fe8.jpg"
+  }
+]);
